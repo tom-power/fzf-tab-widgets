a6f2c55c4f4c905c91b8f5c71690c502b9c62531
Tom Power <tpower21@gmail.com>
2024-05-25 00:44:54 +0100

    split completions/lib, namespace with prefixes

--- fzf-tab-widgets.plugin.zsh
+++ fzf-tab-widgets.plugin.zsh
@@ -2,7 +2,7 @@
 0="${${(M)0:#/*}:-$PWD/$0}"
 FZF_TAB_WIDGETS_HOME="${0:A:h}"
 
-for completion in $(find "$FZF_TAB_WIDGETS_HOME"/completions -type f); do
+for completion in $(find $FZF_TAB_WIDGETS_HOME/completions -type f); do
   source "$completion"
 done
 
